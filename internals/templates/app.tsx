6b5e8830b0e59fd7ef60b07d7626f38ac4c75f1f
linhtran1028 <trankhanhlinh.1028@gmail.com>
2021-10-01 10:03:16 +0700

    Update

--- internals/templates/app.tsx
+++ internals/templates/app.tsx
@@ -82,7 +82,7 @@ if (!(window as any).Intl) {
     .then(() =>
       Promise.all([
         import('intl/locale-data/jsonp/en.js'),
-        import('intl/locale-data/jsonp/de.js'),
+        //import('intl/locale-data/jsonp/de.js'),
       ]),
     )
     .then(() => render(translationMessages))
